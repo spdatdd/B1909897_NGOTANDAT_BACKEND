cc8a932da0d72f918a06ad188cb7444f12b2d3fc
spdatdd <ngod2810@gmail.com>
2023-02-17 15:04:02 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const cors = require("cors")
+const ApiError = require("./app/api-error")
+const contactsRouter = require("./app/routes/contact.route")
 
 const app = express()
 
@@ -8,6 +10,17 @@ app.use(express.json())
 
 app.get("/", (req, res) => {
     res.json({message: "welcome to contact book application."})
+})
+
+app.use("/api/contacts", contactsRouter)
+app.use((req, res, next)=>{
+    return next(new ApiError(404, "Resource not found"))
+})
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "internal server Error"
+    })
 })
 
 module.exports = app
